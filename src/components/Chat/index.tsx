abac06355beb571ae9f83af072710c5ee883a0b5
Jonas <wjlester77@gmail.com>
2024-04-02 21:30:27 +0800

    封裝localStorage2

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,3 +1,4 @@
+import { ChatLogsType } from "@/types";
 import { getCompletion } from "@/utils/getCompletion";
 import { Textarea, Button } from "@mantine/core";
 import { useState } from "react";
@@ -9,18 +10,47 @@ export const Chat = () => {
 
     const [ completion, setCompletion ] = useState('');
 
+    const [ chatList, setChatList ] = useState<ChatLogsType>([]);
+
     const getAIResp = async () => {
+        const list = [
+            ...chatList,
+            {
+                role: 'user',
+                content: prompt
+            }
+        ];
+
+        setChatList(list);
+
         const resp = await getCompletion({
             prompt
         })
         setCompletion(resp.content);
         // console.log(resp);
-        
+        setChatList([
+            ...list,
+            {
+                role: 'assistant',
+                content: resp.content
+            }
+        ]);
     };
 
     return (
     <div className="h-screen items-center flex flex-col">
-        <div>{ completion }</div>
+        <div className="h-[50vh]">
+            {
+                chatList.map((item, idx) => {
+                    return (
+                        <div key={ `${item.role}-${idx}` }>
+                            <div>{ item.role }</div>
+                            <div>{ item.content }</div>
+                        </div>
+                    );
+                })
+            }
+        </div>
         <div className="flex items-center w-3/5">
             <Textarea
                 placeholder="Enter your prompt" 
--- src/utils/ChatStorage.ts
+++ src/utils/ChatStorage.ts
@@ -1,1 +1,39 @@
+import { ChatLogsStorageType, ChatLogsType } from "@/types";
+import { getLocal, setLocal } from "./storage";
+
 // save ChatLogs
+
+const CHAT_LOGS_KEY = 'ai_chatLogs';
+
+export const getChatLogsContainer = () => {
+    let list = getLocal<ChatLogsStorageType>(CHAT_LOGS_KEY);
+    if (!list) {
+        list = {};
+        setLocal(CHAT_LOGS_KEY, list);
+    }
+    return list;
+};
+
+
+export const getChatLogs = (id: string) => {
+    const logs = getChatLogsContainer();
+    return logs[id] || [];
+};
+
+
+export const updateChatLogs = (id: string, log: ChatLogsType) => {
+    const logs = getChatLogsContainer();
+    logs[id] = log;
+    setLocal(CHAT_LOGS_KEY, logs);
+};
+
+
+export const clearChatLogs = (id: string) => {
+    const logs = getChatLogsContainer();
+
+    if (logs[id]) {
+        logs[id] = [];
+    }
+
+    setLocal(CHAT_LOGS_KEY, logs);
+}
