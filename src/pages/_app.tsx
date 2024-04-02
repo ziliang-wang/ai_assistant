7173d0ccd906fc8e200f72f50f5132c9b4d84aca
Jonas <wjlester77@gmail.com>
2024-04-02 11:55:09 +0800

    init2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [ colorScheme, setColorScheme ] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+  }
+
+  return (
+    <ColorSchemeProvider 
+      colorScheme="dark"
+      toggleColorScheme={ toggleColorScheme }
+    >
+      <MantineProvider 
+        theme={{ colorScheme, primaryColor: "green" }}
+        withNormalizeCSS
+        withGlobalStyles
+      >
+        <Notifications position='top-right' zIndex={ 2077 }></Notifications>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
